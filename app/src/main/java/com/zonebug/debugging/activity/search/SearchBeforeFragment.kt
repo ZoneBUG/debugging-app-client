547e20ecd8e9f1d8c4aed206602a6af1759635cd
seoyamin <seoyamin@ewhain.net>
2023-05-18 06:43:03 +0900

    fix : 기타 디자인 수정

--- app/src/main/java/com/zonebug/debugging/activity/search/SearchBeforeFragment.kt
+++ app/src/main/java/com/zonebug/debugging/activity/search/SearchBeforeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatButton
 import com.zonebug.debugging.R
 
@@ -20,12 +21,14 @@ class SearchBeforeFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_search_before, container, false)
+        val container : LinearLayout = view.findViewById(R.id.Search_Before_Container_Image)
         val selectBtn : ImageButton = view.findViewById(R.id.Search_Before_Btn_Select)
         val editBtn : AppCompatButton = view.findViewById(R.id.Search_Before_Btn_Edit)
 
         // 이미지 선택하기
         selectBtn.setOnClickListener {
             (activity as SearchActivity).selectImage()
+            container.setBackgroundResource(R.drawable.custom_button_white)
         }
 
         // 이미지 다시 선택하기
